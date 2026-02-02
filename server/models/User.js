2b3c1af66d6a42ea73dd04936d03e7f22eb8b4be
Aryaman-Saraswat2004 <aryamansaraswat@gmail.com>
2026-02-03 02:43:51 +0530

    env updates

--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,14 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-  role: {
-    type: String,
-    enum: ["USER", "ADMIN", "VOLUNTEER"],
-    default: "USER",
-  },
-  city: String,
-});
-
-module.exports = mongoose.model("User", userSchema);
+const mongoose = require('mongoose') ;
+const UserSchema = new mongoose.Schema({
+    "name" : String, 
+    "email" : String , 
+    "password" : String ,
+    "city" : String ,
+    role : {
+        type : String , 
+        enum : ["USER" , "ADMIN" , "VOLUNTEER"] ,
+        default : "USER"
+    }
+  
+})
+module.exports = mongoose.model("users" , UserSchema) ; 
